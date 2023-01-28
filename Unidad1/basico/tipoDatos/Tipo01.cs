e48415b4c4bc60173cdc9fa28477dfd2cfd93f89
Erick Arita <erick.emao@gmail.com>
2023-01-27 18:57:58 -0600

    pasos iniciales de c#

---
+++ Unidad1/basico/tipoDatos/Tipo01.cs
@@ -0,0 +1,17 @@
+﻿namespace basico.tipoDatos;
+
+public class Tipo01
+{
+    public Tipo01()
+    {
+        string nombre = "Juan";
+        int edad = 20;
+        double estatura = 1.70;
+        bool esMayorDeEdad = edad >= 18;
+        char inicial = nombre[0];
+        decimal precio = 12.50m;
+        float peso = 75.5f;
+        long numero = 1234567890;        
+        
+    }
+}
