664b87c50136e4acba508eb8a324c7b1348dd48b
Erick Arita <erick.emao@gmail.com>
2023-02-02 18:29:19 -0600

    Add[Vocales]: nombres que comienzan con vocales

--- Unidad1/basico/Program.cs
+++ Unidad1/basico/Program.cs
@@ -1,13 +1,15 @@
 ﻿using basico.estructuras_control;
 
 
-int contador = 3;
-decimal[] notas = {0, 0, 0};
+// int contador = 3;
+// decimal[] notas = {0, 0, 0};
+//
+// for (int i = 0; i < 3; i++)
+// {
+//     Console.WriteLine("Ingrese nota: " + (i + 1));
+//     notas[i] = decimal.Parse(Console.ReadLine());
+// }
+//
+// Promedio promedio = new(notas[0], notas[1], notas[2]);
 
-for (int i = 0; i < 3; i++)
-{
-    Console.WriteLine("Ingrese nota: " + (i + 1));
-    notas[i] = decimal.Parse(Console.ReadLine());
-}
-
-Promedio promedio = new(notas[0], notas[1], notas[2]);
+Vocales vocales = new();
---
+++ Unidad1/basico/estructuras-control/Vocales.cs
@@ -0,0 +1,35 @@
+﻿namespace basico.estructuras_control;
+
+public class Vocales
+{
+    public Vocales()
+    {
+        Console.WriteLine("Nombres según la vocal");
+        Console.WriteLine("--------------------------\n");
+        Console.WriteLine("Ingrese una vocal: ");
+
+        char? vocal = Console.ReadLine()?[0];
+
+        switch (vocal)
+        {
+            case 'A':
+                Console.Write("Angie, Ana");
+                break;
+            case 'E':
+                Console.Write("Erick, Ester");
+                break;
+            case 'I':
+                Console.Write("Iván, I");
+                break;
+            case 'O':
+                Console.Write("Oscar, Orlando");
+                break;
+            case 'U':
+                Console.Write("Uriel, El burro eres tú");
+                break;
+            default:
+                Console.WriteLine("No se ingresó una vocal");
+                break;
+        }
+    }
+}
