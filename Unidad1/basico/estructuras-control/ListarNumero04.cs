4d5ad3d6dae3baa41069bd3bd306a4e7ecda81a2
Erick Arita <erick.emao@gmail.com>
2023-02-07 18:29:28 -0600

    Add: listar numeros con ciclo for

---
+++ Unidad1/basico/estructuras-control/ListarNumero04.cs
@@ -0,0 +1,22 @@
+﻿namespace basico.estructuras_control;
+
+public class ListarNumero04
+{
+    public ListarNumero04()
+    {
+        bool continuar;
+        do
+        {
+            Console.WriteLine("Hasta donde quiere contar: ");
+            int hasta = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= hasta; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Quiere seguir contando? (S/N)");
+            continuar = Console.ReadLine()?.ToUpper() == "S";
+        } while (continuar);
+    }
+}
