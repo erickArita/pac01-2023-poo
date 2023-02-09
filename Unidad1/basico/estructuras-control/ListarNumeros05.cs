4d74b20187da264a79bc1f88eb7b8cd911b5da8f
Erick Arita <erick.emao@gmail.com>
2023-02-08 18:44:02 -0600

    fix: generar numeros desde el 1 al 100

---
+++ Unidad1/basico/estructuras-control/ListarNumeros05.cs
@@ -0,0 +1,32 @@
+﻿namespace basico.estructuras_control;
+
+public class ListarNumeros05
+{
+    public ListarNumeros05()
+    {
+        int[] numerosGenerados = new int[10];
+
+
+        Random random = new Random();
+        for (int j = 0; j < 10; j++)
+        {
+            bool yaExiste;
+            int numeroGenerado = random.Next(1, 10);
+            numerosGenerados[j] = numeroGenerado;
+
+            // do
+            // {
+            //     numeroGenerado = random.Next(1, 10);
+            //     for()
+            // } while (yaExiste);
+
+            numerosGenerados[j] = numeroGenerado;
+        }
+
+        foreach (var numerosGenerado in numerosGenerados)
+        {
+            Console.Write(numerosGenerado);
+            Console.Write(" - ");
+        }
+    }
+}
